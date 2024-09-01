ed30fefa9975f9f5cc5c9419d1386d77121cf890
Graham Martin <graham.martin@immediate.co.uk>
2024-09-02 00:27:57 +0100

    Bring all the parts together

--- src/helpers/ga_config.py
+++ src/helpers/ga_config.py
@@ -4,9 +4,5 @@ import os
 def get_ga_properties(filepath = 'src/data/ga-properties.csv'):
     with open(filepath, 'r') as file:
         data_reader = csv.DictReader(file)
-        # mydict = {rows[0]:rows[1] for rows in data_reader}
         data = [row for row in data_reader]
-        # data = []
-        # print(mydict)
-        print(data)
     return data
