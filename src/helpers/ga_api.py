0816960521771afc3639331fda9ca1a58bb51509
Graham Martin <graham.martin@immediate.co.uk>
2024-09-01 23:38:38 +0100

    Cleanup the response from GA into something more usable

--- src/helpers/ga_api.py
+++ src/helpers/ga_api.py
@@ -7,6 +7,7 @@ from google.analytics.data_v1beta.types import (
 )
 
 def build_metrics(metrics_str):
+    # Available metrics: https://developers.google.com/analytics/devguides/reporting/data/v1/api-schema#metrics
     metrics = []
     
     for metric in metrics_str.split(','):
@@ -15,6 +16,7 @@ def build_metrics(metrics_str):
     return metrics
 
 def build_dimensions(dimensions_str):
+    # Available dimensions: https://developers.google.com/analytics/devguides/reporting/data/v1/api-schema#dimensions
     dimensions = []
 
     if(type(dimensions_str) == str):
@@ -28,14 +30,41 @@ def build_report_request(property_id, start_date, end_date, metrics_str, dimensi
         property=f'properties/{property_id}',
         metrics=build_metrics(metrics_str),
         dimensions=build_dimensions(dimensions_str),
-        date_ranges=[DateRange(start_date=start_date, end_date=end_date)],
+        date_ranges=[DateRange(start_date=start_date, end_date=end_date)], # Data range support: https://developers.google.com/analytics/devguides/reporting/data/v1/rest/v1beta/DateRange
     )
+
+def clean_ga_response(response):
+    cleaned_response = []
+
+    for row_index, row in enumerate(response.rows):
+        new_dimensions = {}
+        new_metrics = {}
+
+        for dimension_index, dimension in enumerate(response.dimension_headers):
+            new_dimensions[dimension.name] = row.dimension_values[dimension_index].value
+            
+        for metric_index, metric in enumerate(response.metric_headers):
+            new_metrics[metric.name] = int(row.metric_values[metric_index].value)
+        
+        if(len(new_dimensions) == 0):
+            cleaned_response = cleaned_response + [{ 'metrics': new_metrics }]
+        else:
+            cleaned_response = cleaned_response + [{ 'dimensions': new_dimensions, 'metrics': new_metrics }]    
+
+    return cleaned_response
 
 def generate_ga_report(property_id, start_date, end_date, metrics, dimensions=None):
     client = BetaAnalyticsDataClient()
+
+    # Build a fully formed GA request compatible with GA Data API
     request = build_report_request(property_id, start_date, end_date, metrics, dimensions)
+
+    # Request the report
     response = client.run_report(request)
 
-    return response
+    # Clean the response to suit our needs
+    clean_response = clean_ga_response(response)
 
+    print(clean_response)
 
+    return clean_response
--- tests/test_ga_api.py
+++ tests/test_ga_api.py
@@ -1,8 +1,9 @@
-from helpers.ga_api import build_metrics, build_dimensions, build_report_request, generate_ga_report
-from unittest.mock import patch, call
+from unittest.mock import patch, call, Mock
+from helpers.ga_api import build_metrics, build_dimensions, build_report_request, generate_ga_report, clean_ga_response
+
 
 def test_build_metrics_single():
-    with patch("helpers.ga_api.Metric", return_value='test') as mock_metrics:
+    with patch('helpers.ga_api.Metric', return_value='test') as mock_metrics:
         # Arrange - set up input and expected output
         expected_type = list
         expected_length = 1 
@@ -19,7 +20,7 @@ def test_build_metrics_single():
         mock_metrics.assert_called_once_with(name='one')
 
 def test_build_metrics_multiple():
-    with patch("helpers.ga_api.Metric", return_value='test') as mock_metrics:
+    with patch('helpers.ga_api.Metric', return_value='test') as mock_metrics:
         # Arrange - set up input and expected output
         expected_type = list
         expected_length = 2 
@@ -37,7 +38,7 @@ def test_build_metrics_multiple():
         mock_metrics.assert_has_calls([call(name='one'), call(name='two')])
 
 def test_build_metrics_multiple_with_whitespace():
-    with patch("helpers.ga_api.Metric", return_value='test') as mock_metrics:
+    with patch('helpers.ga_api.Metric', return_value='test') as mock_metrics:
         # Arrange - set up input and expected output
         expected_type = list
         expected_length = 2 
@@ -55,7 +56,7 @@ def test_build_metrics_multiple_with_whitespace():
         mock_metrics.assert_has_calls([call(name='one'), call(name='two')])
 
 def test_build_dimensions_single():
-    with patch("helpers.ga_api.Dimension", return_value='test') as mock_dimensions:
+    with patch('helpers.ga_api.Dimension', return_value='test') as mock_dimensions:
         # Arrange - set up input and expected output
         expected_type = list
         expected_length = 1 
@@ -72,7 +73,7 @@ def test_build_dimensions_single():
         mock_dimensions.assert_called_once_with(name='one')
 
 def test_build_dimensions_multiple():
-    with patch("helpers.ga_api.Dimension", return_value='test') as mock_dimensions:
+    with patch('helpers.ga_api.Dimension', return_value='test') as mock_dimensions:
         # Arrange - set up input and expected output
         expected_type = list
         expected_length = 2 
@@ -90,7 +91,7 @@ def test_build_dimensions_multiple():
         mock_dimensions.assert_has_calls([call(name='one'), call(name='two')])
 
 def test_build_dimensions_multiple_with_whitespace():
-    with patch("helpers.ga_api.Dimension", return_value='test') as mock_dimensions:
+    with patch('helpers.ga_api.Dimension', return_value='test') as mock_dimensions:
         # Arrange - set up input and expected output
         expected_type = list
         expected_length = 2 
@@ -108,9 +109,9 @@ def test_build_dimensions_multiple_with_whitespace():
         mock_dimensions.assert_has_calls([call(name='one'), call(name='two')])
 
 def test_build_report_request():
-    with patch("helpers.ga_api.RunReportRequest", return_value=True) as mock_report_request:
-        with patch("helpers.ga_api.Metric", return_value='test-metric'):
-            with patch("helpers.ga_api.DateRange", return_value='test-dates'):
+    with patch('helpers.ga_api.RunReportRequest', return_value=True) as mock_report_request:
+        with patch('helpers.ga_api.Metric', return_value='test-metric'):
+            with patch('helpers.ga_api.DateRange', return_value='test-dates'):
                 # Arrange - set up input and expected output
                 expected_type = bool
                 expected_response = True
@@ -136,10 +137,10 @@ def test_build_report_request():
                 assert mock_report_request.call_args == expected_call
 
 def test_build_report_request_with_dimensions():
-    with patch("helpers.ga_api.RunReportRequest", return_value=True) as mock_report_request:
-        with patch("helpers.ga_api.Metric", return_value='test-metric'):
-            with patch("helpers.ga_api.Dimension", return_value='test-dimension'):
-                with patch("helpers.ga_api.DateRange", return_value='test-dates'):
+    with patch('helpers.ga_api.RunReportRequest', return_value=True) as mock_report_request:
+        with patch('helpers.ga_api.Metric', return_value='test-metric'):
+            with patch('helpers.ga_api.Dimension', return_value='test-dimension'):
+                with patch('helpers.ga_api.DateRange', return_value='test-dates'):
                     # Arrange - set up input and expected output
                     expected_type = bool
                     expected_response = True
@@ -166,8 +167,8 @@ def test_build_report_request_with_dimensions():
                     assert mock_report_request.call_args == expected_call
 
 def test_generate_ga_report():
-    with patch("helpers.ga_api.BetaAnalyticsDataClient") as mock_beta_analytics_data_client:
-        with patch("helpers.ga_api.BetaAnalyticsDataClient.run_report") as mock_run_report:
+    with patch('helpers.ga_api.BetaAnalyticsDataClient') as mock_beta_analytics_data_client:
+        with patch('helpers.ga_api.BetaAnalyticsDataClient.run_report') as mock_run_report:
         
             # Arrange - set up input and expected output
             expected_type = list
@@ -191,3 +192,131 @@ def test_generate_ga_report():
             assert type(report) == expected_type
             assert len(report) == expected_length
             assert report == expected_response
+
+def test_clean_ga_response_with_dimensions():
+    # Arrange - set up input and expected output
+    expected_type = list
+    expected_dimension_length = 1
+    expected_dimension_name = 'mock_dimension'
+    expected_dimension_value = 'test_dimension_value'
+    expected_metric_name = 'mock_metric'
+    expected_metric_value = 100
+    expected_metric_type = int
+
+    # GA Response is a heavily nested class – mocking each element of this 
+    mockResponse = Mock()
+    mockRow = Mock()
+    mockMetricHeader = Mock()
+    mockDimensionHeader = Mock()
+    mockMetricValue = Mock()
+    mockDimensionValue = Mock()
+
+    mockMetricHeader.name = expected_metric_name
+    mockDimensionHeader.name = expected_dimension_name
+    mockMetricValue.value = expected_metric_value
+    mockDimensionValue.value = expected_dimension_value
+    mockRow.dimension_values = [mockDimensionValue]
+    mockRow.metric_values = [mockMetricValue]
+    mockResponse.dimension_headers = [mockDimensionHeader]
+    mockResponse.metric_headers = [mockMetricHeader]
+    mockResponse.rows = [mockRow]
+    mockResponse.kind = 'mock_kind'
+    
+    # Act
+    cleaned_response = clean_ga_response(mockResponse)
+
+    # Assert
+    assert type(cleaned_response) == expected_type
+    assert expected_dimension_name in cleaned_response[0]['dimensions']
+    assert len(cleaned_response[0]['dimensions']) == expected_dimension_length
+    assert cleaned_response[0]['dimensions'][expected_dimension_name] == expected_dimension_value
+    assert expected_metric_name in cleaned_response[0]['metrics']
+    assert cleaned_response[0]['metrics'][expected_metric_name] == expected_metric_value
+    assert type(cleaned_response[0]['metrics'][expected_metric_name]) == expected_metric_type
+    assert 'kind' not in cleaned_response[0]
+
+def test_clean_ga_response_with_multiple_dimensions():
+    # Arrange - set up input and expected output
+    expected_type = list
+    expected_dimension_length = 2
+    expected_dimension_name = 'mock_dimension'
+    expected_dimension_name_2 = 'another_mock_dimension'
+    expected_dimension_value = 'test_dimension_value'
+    expected_metric_name = 'mock_metric'
+    expected_metric_value = 100
+    expected_metric_type = int
+
+    # GA Response is a heavily nested class – mocking each element of this 
+    mockResponse = Mock()
+    mockRow = Mock()
+    mockMetricHeader = Mock()
+    mockDimensionHeader = Mock()
+    mockDimensionHeader2 = Mock()
+    mockMetricValue = Mock()
+    mockDimensionValue = Mock()
+
+    mockMetricHeader.name = expected_metric_name
+    mockDimensionHeader.name = expected_dimension_name
+    mockDimensionHeader2.name = expected_dimension_name_2
+    mockMetricValue.value = expected_metric_value
+    mockDimensionValue.value = expected_dimension_value
+    mockRow.dimension_values = [mockDimensionValue, mockDimensionValue]
+    mockRow.metric_values = [mockMetricValue]
+    mockResponse.dimension_headers = [mockDimensionHeader,mockDimensionHeader2]
+    mockResponse.metric_headers = [mockMetricHeader]
+    mockResponse.rows = [mockRow]
+    mockResponse.kind = 'mock_kind'
+    
+    # Act
+    cleaned_response = clean_ga_response(mockResponse)
+
+    # Assert
+    assert type(cleaned_response) == expected_type
+    assert expected_dimension_name in cleaned_response[0]['dimensions']
+    assert expected_dimension_name_2 in cleaned_response[0]['dimensions']
+    assert len(cleaned_response[0]['dimensions']) == expected_dimension_length
+    assert cleaned_response[0]['dimensions'][expected_dimension_name] == expected_dimension_value
+    assert cleaned_response[0]['dimensions'][expected_dimension_name_2] == expected_dimension_value
+    assert expected_metric_name in cleaned_response[0]['metrics']
+    assert cleaned_response[0]['metrics'][expected_metric_name] == expected_metric_value
+    assert type(cleaned_response[0]['metrics'][expected_metric_name]) == expected_metric_type
+    assert 'kind' not in cleaned_response[0]
+
+def test_clean_ga_response_without_dimensions():
+    # Arrange - set up input and expected output
+    expected_type = list
+    expected_dimension_name = 'mock_dimension'
+    expected_dimension_value = 'test_dimension_value'
+    expected_metric_name = 'mock_metric'
+    expected_metric_value = 100
+    expected_metric_type = int
+
+    # GA Response is a heavily nested class – mocking each element of this 
+    mockResponse = Mock()
+    mockRow = Mock()
+    mockMetricHeader = Mock()
+    mockDimensionHeader = Mock()
+    mockMetricValue = Mock()
+    mockDimensionValue = Mock()
+
+    mockMetricHeader.name = expected_metric_name
+    mockDimensionHeader.name = expected_dimension_name
+    mockMetricValue.value = expected_metric_value
+    mockDimensionValue.value = expected_dimension_value
+    mockRow.metric_values = [mockMetricValue]
+    mockResponse.dimension_headers = []
+    mockResponse.metric_headers = [mockMetricHeader]
+    mockResponse.rows = [mockRow]
+    mockResponse.kind = 'mock_kind'
+    
+    # Act
+    cleaned_response = clean_ga_response(mockResponse)
+
+    # Assert
+    assert type(cleaned_response) == expected_type
+    assert 'dimensions' not in cleaned_response[0]
+    assert expected_metric_name in cleaned_response[0]['metrics']
+    assert cleaned_response[0]['metrics'][expected_metric_name] == expected_metric_value
+    assert type(cleaned_response[0]['metrics'][expected_metric_name]) == expected_metric_type
+    assert 'kind' not in cleaned_response[0]
+
