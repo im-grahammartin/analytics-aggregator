6545fdedab2f177142966b4df1cc1b87f689c57c
Graham Martin <graham.martin@immediate.co.uk>
2024-09-03 12:05:10 +0100

    Add additional comments to aggregator to improve readability

--- src/transformers/aggregator.py
+++ src/transformers/aggregator.py
@@ -1,4 +1,5 @@
-
+# TODO: Refactor this module to reduct the complexity in this logic
+# Aggregate reports takes in an array of results and merges them into a object adding metrics together to give a single set of results
 def aggregate_reports(data):
     aggregate = {}
 
@@ -6,6 +7,7 @@ def aggregate_reports(data):
         for result in row['results']:
             aggregate_key = ''
 
+            # If dimensions are included then join the keys from each into a single flattened string
             if('dimensions' in result):
                 for key in result['dimensions']:
                     aggregate_key+= result['dimensions'][key] + '|'
@@ -15,12 +17,15 @@ def aggregate_reports(data):
                 if(aggregate_key not in aggregate):
                     aggregate[aggregate_key] = {}
                 
+                # If a metric is already in the aggregate dictionary then add the values together, otherwise add it as a new result
                 for key in result['metrics']:
                     if(key in aggregate[aggregate_key]):
                         aggregate[aggregate_key][key] += result['metrics'][key]
                     else:
                         aggregate[aggregate_key][key] = result['metrics'][key]
             else:
+                # Dimensions are optional, this logic handles results without dimensions
+                # If a metric is already in the aggregate dictionary then add the values together, otherwise add it as a new result
                 for key in result['metrics']:
                     if(key in aggregate):
                         aggregate[key] += result['metrics'][key]
